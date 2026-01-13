c07ba2b33aca66f06b667313c3d98e46c1afb51a
HanyHagar <hanymagdyhagar@gmail.com>
2026-01-14 01:54:26 +0200

    feat(settings): add settings cubit

--- lib/core/settings/cubits/settings_cubit.dart
+++ lib/core/settings/cubits/settings_cubit.dart
@@ -1,10 +1,19 @@
+import 'settings_states.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import '../../services/shared_preferences_services.dart';
 
-class SettingsCubit extends Cubit<void> {
+class SettingsCubit extends Cubit<SettingsStates> {
   final SharedPreferencesService _pref;
 
-  SettingsCubit(this._pref) : super(null);
+  SettingsCubit(this._pref) : super(SettingsInitialState());
+
+  // ===== Data =====
+  ThemeMode theme = ThemeMode.system;
+  String language = 'en';
+  bool notifications = true;
+  bool firstLaunch = true;
+  bool isLogin = false;
 
   // ===== Keys =====
   static const _themeKey = 'theme_mode';
@@ -13,30 +22,91 @@ class SettingsCubit extends Cubit<void> {
   static const _firstLaunchKey = 'first_launch';
   static const _loginKey = 'is_login';
 
+  // ===== Constants =====
+
+  static const Map<String, ThemeMode> themes = {
+    'Light': ThemeMode.light,
+    'Dark': ThemeMode.dark,
+    'System': ThemeMode.system,
+  };
+  static const Map<String, String> languages = {
+    'English': 'en',
+    'Arabic': 'ar',
+  };
+
   // ===== Getters (SOURCE OF TRUTH) =====
-  String get themeMode => _pref.getString(_themeKey, defaultValue: 'system');
 
-  String get languageCode => _pref.getString(_languageKey, defaultValue: 'en');
-
-  bool get notificationsEnabled =>
-      _pref.getBool(_notificationsKey, defaultValue: true);
-
-  bool get isFirstLaunch => _pref.getBool(_firstLaunchKey, defaultValue: true);
-
-  bool get isLogin => _pref.getBool(_loginKey, defaultValue: false);
+  void loadSettings() {
+    try {
+      theme =
+          themes[_pref.getString(_themeKey, defaultValue: 'System')] ??
+          ThemeMode.system;
+      language =
+          languages[_pref.getString(_languageKey, defaultValue: 'English')] ??
+          'en';
+      notifications = _pref.getBool(_notificationsKey, defaultValue: true);
+      firstLaunch = _pref.getBool(_firstLaunchKey, defaultValue: true);
+      isLogin = _pref.getBool(_loginKey, defaultValue: false);
+      emit(SettingsInitialState());
+    } catch (e) {
+      emit(SettingsErrorState(e.toString()));
+    }
+  }
 
   // ===== Setters =====
-  Future<void> setThemeMode(String value) async =>
-      _pref.setString(_themeKey, value);
 
-  Future<void> setLanguage(String value) async =>
-      _pref.setString(_languageKey, value);
+  Future<void> setThemeMode(String value) async {
+    try {
+      await _pref.setString(_themeKey, value);
+      theme = themes[value] ?? ThemeMode.system;
+      emit(SettingsChangeModeState());
+    } catch (e) {
+      emit(SettingsErrorState(e.toString()));
+    }
+  }
 
-  Future<void> setNotifications(bool value) async =>
-      _pref.setBool(_notificationsKey, value);
+  Future<void> setLanguage(String value) async {
+    try {
+      await _pref.setString(_languageKey, value);
+      language = languages.entries
+          .firstWhere(
+            (element) => element.value == value,
+            orElse: () => const MapEntry('English', 'en'),
+          )
+          .value;
+      emit(SettingsChangeLanguageState());
+    } catch (e) {
+      emit(SettingsErrorState(e.toString()));
+    }
+  }
 
-  Future<void> setFirstLaunch(bool value) async =>
-      _pref.setBool(_firstLaunchKey, value);
+  Future<void> setNotifications(bool value) async {
+    try {
+      await _pref.setBool(_notificationsKey, value);
+      notifications = value;
+      emit(SettingsChangeNotificationsState());
+    } catch (e) {
+      emit(SettingsErrorState(e.toString()));
+    }
+  }
 
-  Future<void> setLogin(bool value) async => _pref.setBool(_loginKey, value);
+  Future<void> setFirstLaunch(bool value) async {
+    try {
+      await _pref.setBool(_firstLaunchKey, value);
+      firstLaunch = value;
+      emit(SettingsChangeFirstLaunchState());
+    } catch (e) {
+      emit(SettingsErrorState(e.toString()));
+    }
+  }
+
+  Future<void> setLogin(bool value) async {
+    try {
+      await _pref.setBool(_loginKey, value);
+      isLogin = value;
+      emit(SettingsChangeLoginState());
+    } catch (e) {
+      emit(SettingsErrorState(e.toString()));
+    }
+  }
 }
